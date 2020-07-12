868881549bef13e8ef252094b7d65070e322c45c
Luteranya Nyange <dieume.dev@gmail.com>
2020-07-12 20:07:28 +0300

    porfolio

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 
 
-def create_app():
+def create_app(**override_config):
     app = Flask(__name__)
 
+    # Application configuration
     app.config.from_pyfile('settings.py')
+
+    # import Blueprints
+    from app.pages.views import pages
+
+    # Register Blueprints
+    app.register_blueprint(pages)
 
     return app
--- app/settings.py
+++ app/settings.py
@@ -3,3 +3,4 @@ import os
 FLASK_APP = os.environ.get('FLASK_APP')
 FLASK_ENV = os.environ.get('FLASK_ENV')
 FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
+host = '0.0.0.0'
